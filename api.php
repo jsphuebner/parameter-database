eb71b28b73181d6c65a7e85e2f98522fd212fb34
Dima <dima@Workgroup.local>
2020-11-01 17:47:35 -0800

    functional API

--- api.php
+++ api.php
@@ -15,16 +15,43 @@ if(isset($_GET['offset']))
 if(isset($_GET['id']))
 {
 	$id = $_GET['id'];
-	$metadata = $sqlDrv->mapQuery("SELECT name,value FROM pd_namedmetadata WHERE id=$id", "name");
-	$data = $sqlDrv->arrayQuery("SELECT category, name, unit, value FROM pd_namedata WHERE setid=$id LIMIT $OFFSET, $QUERYLIMIT");
 
-	echo json_encode(array_merge($metadata,$data));
+	if(isset($_GET['metadata']))
+	{
+		$metadata = $sqlDrv->mapQuery("SELECT name,value FROM pd_namedmetadata WHERE id=$id", "name");
+		echo json_encode($metadata);
+	}else{
+		$data = $sqlDrv->arrayQuery("SELECT category, name, unit, value FROM pd_namedata WHERE setid=$id");
+		echo json_encode($data);
+	}
 
 }else{
 
-	$rows = $sqlDrv->arrayQuery("SELECT id, name, value FROM pd_namedmetadata LIMIT $OFFSET, $QUERYLIMIT");
+	//$rows = $sqlDrv->arrayQuery("SELECT id, name, value FROM pd_namedmetadata LIMIT $OFFSET, $QUERYLIMIT");
+	$rows = $sqlDrv->arrayQuery("SELECT 
+	        `d`.`id` AS `id`,
+	        `m`.`metaitem` AS `metaitem`,
+	        `m`.`value` AS `value`
+	    FROM
+	        (`pd_datasets` `d` JOIN `pd_metadata` `m`)
+	    WHERE
+	        (`d`.`metadata` = `m`.`setid`)  AND (`m`.`metaitem` IN (2 , 5, 6)) LIMIT $OFFSET, $QUERYLIMIT");
 
-	echo json_encode($rows);
+	$lastId = 0;
+	$data = [];
+	
+	foreach ($rows as $row)
+	{
+		if ($lastId != $row['id'])
+		{
+			array_push($data, ['id' => intval($row['id'])]);
+		}
+		$data[sizeof($data)-1] += [$row['metaitem'] => $row['value']];
+		
+		$lastId = $row['id'];
+	}
+	
+	echo json_encode($data);
 }
 
 ?>
